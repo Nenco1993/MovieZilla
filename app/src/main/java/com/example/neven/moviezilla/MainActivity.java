f38ad8ac78f6c2ab4990a7289c55afa1dd3c8551
Neven <nevenmagdalenic1993@gmail.com>
2017-01-17 16:13:29 +0100

    first commit

---
+++ app/src/main/java/com/example/neven/moviezilla/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.neven.moviezilla;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
